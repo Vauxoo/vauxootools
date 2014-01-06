9425ba51987cbad15aa54e8cc711f41a6a5849c6
nhomar <nhomar@vauxoo.com>
2014-01-05 21:56:58 -0430

    Better doc, some pylint improvements, managing error when you ask for an option.

--- vauxootools/__init__.py
+++ vauxootools/__init__.py
@@ -4,3 +4,5 @@
 __author__ = 'Nhomar Hernandez'
 __email__ = 'nhomar@gmail.com'
 __version__ = '0.1.0'
+__doc__ = '''This is the base module to no re-implement some python concepts in
+our libraries to give our customers the best quality and secure service.'''
