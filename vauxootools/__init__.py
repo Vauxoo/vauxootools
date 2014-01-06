8fee60e68bf621cea0546d6c9dd5c5e0c0246a6f
nhomar <nhomar@vauxoo.com>
2014-01-06 00:57:13 -0430

    Added some wrappers to get the config values, improve help

--- vauxootools/__init__.py
+++ vauxootools/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''Just becoming this in a python module with the global variables working
+'''
 
 __author__ = 'Nhomar Hernandez'
 __email__ = 'nhomar@gmail.com'
 __version__ = '0.1.0'
-__doc__ = '''This is the base module to no re-implement some python concepts in
-our libraries to give our customers the best quality and secure service.'''
